80f8f5617dbb6ae2b10da253a8c15770938202a0
alexey-kartynnik <ererf$43eeEwQ>
2021-01-27 00:13:00 +0300

    Add Auth0 UI authentication

--- Pds/Pds.Web/Program.cs
+++ Pds/Pds.Web/Program.cs
@@ -17,7 +17,13 @@ namespace Pds.Web
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddOidcAuthentication(options =>
+            {
+                builder.Configuration.Bind("Auth0", options.ProviderOptions);
+                options.ProviderOptions.ResponseType = "code";
+            });
 
             await builder.Build().RunAsync();
         }
