374949b1265a1900f9ad13dcf70f71eb43acf4aa
alexey-kartynnik <ererf$43eeEwQ>
2021-01-27 00:40:28 +0300

    Blazor UI fetch data from external API now

--- Pds/Pds.Api/Controllers/WeatherForecastController.cs
+++ Pds/Pds.Api/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Logging;
 namespace Pds.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("weather")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
--- Pds/Pds.Api/Startup.cs
+++ Pds/Pds.Api/Startup.cs
@@ -1,15 +1,10 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
 namespace Pds.Api
@@ -26,6 +21,13 @@ namespace Pds.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("PdsUiPolicy",
+                    builder => builder.WithOrigins(Configuration.GetSection("AllowedOrigins").Get<List<string>>().ToArray())
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
             services.AddControllers();
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo {Title = "Pds.Api", Version = "v1"}); });
         }
@@ -40,6 +42,7 @@ namespace Pds.Api
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Pds.Api v1"));
             }
 
+            app.UseCors("PdsUiPolicy");
             app.UseHttpsRedirection();
 
             app.UseRouting();
