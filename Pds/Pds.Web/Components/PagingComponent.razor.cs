c0a980722bfd2cecbd77c18339b91766b639f065
Алексей Перепелкин <catdog50rus@gmail.com>
2021-03-23 11:45:11 +0300

    Реализация выбора количества элементов на странице
    1. Реализация выбора количества элементов на странице.
    2. Пробная реализация тела запроса всех Persons .
    3. Добавление в startup использование статических файлов для компиляции индивидуальных CSS.
    4. Табличный компонент приведен к обобщенному типу.

--- Pds/Pds.Web/Components/PagingComponent.razor.cs
+++ Pds/Pds.Web/Components/PagingComponent.razor.cs
@@ -7,31 +7,49 @@ namespace Pds.Web.Components
     public class PagingComponentBase : ComponentBase
     {
         [Parameter]
-        public EventCallback<int> Pagination { get; set; }
+        public EventCallback<PaginationSettings> Pagination { get; set; }
 
         [Parameter]
-        public int PageSize { get; set; }
+        public int[] PageSizeList { get; set; }
         [Parameter]
         public int TotalItems { get; set; }
         [Parameter]
         public int Radius { get; set; }
 
-        private int pageOffset = 0;
-        private int currentPage = 1;
-        private int totalPages => (int)Math.Ceiling(TotalItems / (double)PageSize);
+        private int pageOffset;
+        private int currentPage;
+        private int totalPages;
 
         protected List<PageModel> pages;
+        private int currentPageSize;
+
+        protected override void OnInitialized()
+        {
+            currentPageSize = PageSizeList[0];
+            currentPage = 1;
+        }
 
         protected override void OnParametersSet()
         {
+            totalPages = (int)Math.Ceiling(TotalItems / (double)currentPageSize);
+            if (totalPages <= 1)
+                currentPage = 1;
             LoadPages();
+        }
+
+        protected void OnSelectPageSize(ChangeEventArgs e)
+        {
+            currentPageSize = int.Parse(e.Value.ToString());
+            SetDefaultPagination();
+            var res = new PaginationSettings(currentPageSize, pageOffset);
+            int[] result = new int[] { 0, currentPageSize };
+            Pagination.InvokeAsync(res);
         }
 
         protected void SelectedPage(PageModel page)
         {
             if (page.Page == currentPage)
             {
-                Console.WriteLine("Current");
                 return;
             }
 
@@ -39,16 +57,25 @@ namespace Pds.Web.Components
             {
                 return;
             }
-
             currentPage = page.Page;
-            pageOffset = (currentPage - 1) * PageSize;
-            Pagination.InvokeAsync(pageOffset);
+
+            pageOffset = (currentPage - 1) * currentPageSize;
+            var res = new PaginationSettings(currentPageSize, pageOffset);
+            int[] result = new int[] { pageOffset, currentPageSize };
+            Pagination.InvokeAsync(res);
+        }
+
+        private void SetDefaultPagination()
+        {
+            currentPage = 1;
+            LoadPages();
         }
 
         private void LoadPages()
         {
             pages = new List<PageModel>();
-            var isPreviousPageLinkEnabled = currentPage != 1;
+            var isPreviousPageLinkEnabled = currentPage > 1 && totalPages > 0;
+
             var previousPage = currentPage - 1;
             pages.Add(new PageModel(previousPage, isPreviousPageLinkEnabled, "Previous"));
 
@@ -60,7 +87,7 @@ namespace Pds.Web.Components
                 }
             }
 
-            var isNextPageLinkEnabled = currentPage != totalPages;
+            var isNextPageLinkEnabled = currentPage < totalPages && totalPages > 0;
             var nextPage = currentPage + 1;
             pages.Add(new PageModel(nextPage, isNextPageLinkEnabled, "Next"));
         }
@@ -84,4 +111,17 @@ namespace Pds.Web.Components
             }
         }
     }
+
+    public struct PaginationSettings
+    {
+        public int PageSize { get; }
+        public int PageOffSet { get; }
+
+        internal PaginationSettings(int pageSize, int pageOffSet)
+        {
+            PageSize = pageSize;
+            PageOffSet = pageOffSet;
+        }
+    }
 }
+
