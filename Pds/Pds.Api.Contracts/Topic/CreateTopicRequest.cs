c11d528c81a66b16756d8e8fbfe93f739c5a0076
Igor <ntehno@mail.ru>
2021-04-02 13:47:02 +0500

    Create TopicController with Create Action

---
+++ Pds/Pds.Api.Contracts/Topic/CreateTopicRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Pds.Api.Contracts.Person;
+
+namespace Pds.Api.Contracts.Topic
+{
+    public class CreateTopicRequest
+    {
+        [Required, StringLength(300, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public ICollection<PersonDto> People { get; set; }
+    }
+}
---
+++ Pds/Pds.Api/Controllers/TopicController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Pds.Api.Contracts.Topic;
+using Pds.Data.Entities;
+using Pds.Services.Interfaces;
+
+namespace Pds.Api.Controllers
+{
+    [ApiController]
+    [Route("api/topics")]
+    public class TopicController : ApiControllerBase
+    {
+        private readonly ITopicService topicService;
+        private readonly IMapper mapper;
+
+        public TopicController(ITopicService topicService, IMapper mapper)
+        {
+            this.topicService = topicService;
+            this.mapper = mapper;
+        }
+
+        [ProducesResponseType(typeof(CreateTopicResponse), StatusCodes.Status200OK)]
+        public async Task<IActionResult> CreateTopic(CreateTopicRequest request)
+        {
+            try
+            {
+                var mappedTopic = mapper.Map<Topic>(request);
+                var result = await topicService.CreateAsync(mappedTopic);
+                return Ok(new CreateTopicResponse(result));
+            }
+            catch (Exception exception)
+            {
+                return ExceptionResult(exception);
+            }
+        }
+    }
+}
