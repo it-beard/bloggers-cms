271709b4211747a0ff85b838975b167a1662f289
Igor <ntehno@mail.ru>
2021-04-02 13:13:21 +0500

    Create Topic-related repositories

---
+++ Pds/Pds.Data/Repositories/TopicRepository.cs
@@ -0,0 +1,12 @@
+﻿using Pds.Data.Entities;
+using Pds.Data.Repositories.Interfaces;
+
+namespace Pds.Data.Repositories
+{
+    public class TopicRepository : RepositoryBase<Topic>, ITopicRepository
+    {
+        public TopicRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
