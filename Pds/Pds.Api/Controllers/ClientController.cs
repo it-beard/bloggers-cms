a10c261d3c3ee68d5a0e04209a05683949aef9d9
Trufanov Roman <pingvin1308@gmail.com>
2021-04-03 19:20:46 +0300

    feat (Person): add sorting to web ui

--- Pds/Pds.Api/Controllers/ClientController.cs
+++ Pds/Pds.Api/Controllers/ClientController.cs
@@ -1,8 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using AutoMapper;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
