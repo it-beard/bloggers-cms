2cf3ced1ced9de9ea5bb77358a0b6c3cb9794a02
Alexander <Alex2021cyberhuman@gmail.com>
2021-04-04 19:23:56 +0500

    Fix wrong person topic creating

--- Pds/Pds.Services/Services/TopicService.cs
+++ Pds/Pds.Services/Services/TopicService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using Pds.Data;
 using Pds.Data.Entities;
@@ -20,7 +21,10 @@ namespace Pds.Services.Services
         public async Task<Guid> CreateAsync(Topic topic)
         {
             topic.CreatedAt = DateTime.UtcNow;
+            var personTopics = topic.PersonTopics;
+            topic.PersonTopics = null;
             var createdTopic = await unitOfWork.Topics.InsertAsync(topic);
+            await RecreatePersonTopics(topic.Id, personTopics);
             return createdTopic.Id;
         }
 
@@ -37,8 +41,42 @@ namespace Pds.Services.Services
         public async Task<Guid> UpdateAsync(Topic topic)
         {
             topic.UpdatedAt = DateTime.UtcNow;
+            var personTopics = topic.PersonTopics;
+            await RecreatePersonTopics(topic.Id, personTopics);
+            topic.PersonTopics = null;
             var updatedTopic = await unitOfWork.Topics.UpdateAsync(topic);
             return updatedTopic.Id;
+        }
+
+        private async Task RecreatePersonTopics(Guid topicId, IEnumerable<PersonTopic> personTopics)
+        {
+            var newPersonTopics =  await RestorePersonTopics(topicId, personTopics);
+            var oldPersonTopics = await unitOfWork.PersonTopics.FindAllByWhereAsync(pt => topicId == pt.TopicId);
+            var removes = !oldPersonTopics.Any()
+                ? new List<PersonTopic>(0)
+                : oldPersonTopics.Where(oldPersonTopic => !newPersonTopics.Contains(oldPersonTopic))
+                    .ToList();
+            await unitOfWork.PersonTopics.DeleteRange(removes);
+            var inserts = !oldPersonTopics.Any()
+                ? newPersonTopics
+                : newPersonTopics.Where(newPersonTopic => !oldPersonTopics.Contains(newPersonTopic))
+                    .ToList();
+            await unitOfWork.PersonTopics.InsertRangeAsync(inserts);
+            
+        }
+
+        private async Task<IList<PersonTopic>> RestorePersonTopics(Guid topicId, IEnumerable<PersonTopic> personTopics)
+        {
+            var list = new List<PersonTopic>();
+            foreach (var personTopic in personTopics)
+            {
+                var firstPerson = await unitOfWork.Persons
+                    .GetFirstWhereAsync(person => personTopic.PersonId == person.Id);
+                if (firstPerson is not null)
+                    list.Add(new PersonTopic(topicId, firstPerson.Id));
+            }
+
+            return list;
         }
     }
 }
