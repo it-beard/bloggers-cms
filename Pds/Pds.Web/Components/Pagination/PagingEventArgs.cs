16a9642097f540cb999fe8b29feb5f8ac906513f
Алексей Перепелкин <catdog50rus@gmail.com>
2021-04-09 17:37:11 +0300

    C# код перенесен в компонент. Вспомогательные классы выделены в отдельные файлы

---
+++ Pds/Pds.Web/Components/Pagination/PagingEventArgs.cs
@@ -0,0 +1,14 @@
+﻿namespace Pds.Web.Components
+{
+    public struct PagingEventArgs
+    {
+        public int PageSize { get; }
+        public int PageOffSet { get; }
+
+        public PagingEventArgs(int pageSize, int pageOffSet)
+        {
+            PageSize = pageSize;
+            PageOffSet = pageOffSet;
+        }
+    }
+}
