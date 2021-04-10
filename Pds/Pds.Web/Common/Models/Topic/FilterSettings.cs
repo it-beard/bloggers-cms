1c03f8f5acfaf241e9bc134df67a0e2e3f0b2b39
Alexander <Alex2021cyberhuman@gmail.com>
2021-04-10 14:06:55 +0500

    refactor(#30 Topic Page): Move TopicStatusFilterItem to other file
    Signed-off-by: Alexander <Alex2021cyberhuman@gmail.com>

--- Pds/Pds.Web/Common/Models/Topic/FilterSettings.cs
+++ Pds/Pds.Web/Common/Models/Topic/FilterSettings.cs
@@ -1,67 +1,44 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using Pds.Core.Enums;
-using Pds.Data.Entities;
 
 namespace Pds.Web.Common.Models.Topic
 {
     public class FilterSettings : IValidatableObject
     {
-        [Range(0, int.MaxValue)]
-        public int? MinRateSum { get; set; }
-        
-        [Range(0, int.MaxValue)]
-        public int? MaxRateSum { get; set; }
-        
-        [Range(0d, 100d)]
-        public double? MinRateAverage { get; set; }
-        
-        [Range(0d, 100d)]
-        
-        public double? MaxRateAverage { get; set; }
-        
-        [Range(0, int.MaxValue)]
-        public int? MinPeopleCount { get; set; }
+        [Range(0, int.MaxValue)] public int? MinRateSum { get; set; }
 
-        [Range(0, int.MaxValue)]
-        public int? MaxPeopleCount { get; set; }
-        
+        [Range(0, int.MaxValue)] public int? MaxRateSum { get; set; }
+
+        [Range(0d, 100d)] public double? MinRateAverage { get; set; }
+
+        [Range(0d, 100d)] public double? MaxRateAverage { get; set; }
+
+        [Range(0, int.MaxValue)] public int? MinPeopleCount { get; set; }
+
+        [Range(0, int.MaxValue)] public int? MaxPeopleCount { get; set; }
+
         public string NameFilter { get; set; }
 
         public IReadOnlyCollection<TopicStatusFilterItem> TopicStatusFilter { get; set; }
-            
-        
+
+
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
             if (MinRateSum > MaxRateSum)
                 yield return new ValidationResult("Invalid Rate Sum filter value",
                     Enumerable.Repeat(nameof(MinRateSum),
                         1));
-            
+
             if (MinRateAverage > MaxRateAverage)
                 yield return new ValidationResult("Invalid Rate Average filter value",
                     Enumerable.Repeat(nameof(MinRateAverage),
                         1));
-            
+
             if (MinPeopleCount > MaxPeopleCount)
                 yield return new ValidationResult("Invalid People Count filter value",
                     Enumerable.Repeat(nameof(MinPeopleCount),
                         1));
         }
     }
-
-    public class TopicStatusFilterItem
-    {
-        public TopicStatusFilterItem(TopicStatus status, bool isSelected)
-        {
-            Status = status;
-            IsSelected = isSelected;
-        }
-
-        public TopicStatus Status { get; set; }
-
-        public bool IsSelected { get; set; }
-    }
-    
 }
---
+++ Pds/Pds.Web/Common/Models/Topic/TopicStatusFilterItem.cs
@@ -0,0 +1,17 @@
+﻿using Pds.Core.Enums;
+
+namespace Pds.Web.Common.Models.Topic
+{
+    public class TopicStatusFilterItem
+    {
+        public TopicStatusFilterItem(TopicStatus status, bool isSelected)
+        {
+            Status = status;
+            IsSelected = isSelected;
+        }
+
+        public TopicStatus Status { get; set; }
+
+        public bool IsSelected { get; set; }
+    }
+}
