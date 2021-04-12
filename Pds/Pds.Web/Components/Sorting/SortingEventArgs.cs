c38d331a46a1ecaaf67852df4156a9b0c8157f14
Aleksei Perepelkin <catdog50rus@gmail.com>
2021-04-12 09:34:35 +0300

    Remove unused components Add C# code to body SortingComponent

---
+++ Pds/Pds.Web/Components/Sorting/SortingEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using Pds.Api.Contracts.Person;
+
+namespace Pds.Web.Components.Sorting
+{
+    public struct SortingEventArgs
+    {
+        public SortingEventArgs(PersonsFieldName personsFieldName, bool ascending)
+        {
+            FieldName = personsFieldName;
+            Ascending = ascending;
+        }
+
+        public PersonsFieldName FieldName { get; }
+        public bool Ascending { get; }
+    }
+}
