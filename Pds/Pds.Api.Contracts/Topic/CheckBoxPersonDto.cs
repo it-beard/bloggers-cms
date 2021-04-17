db9497c741d049be21f0d24dcfc495888990f623
Alexander <Alex2021cyberhuman@gmail.com>
2021-04-17 16:05:33 +0500

    feat(#30 TopicPage): Update topic info page
    1. Move pagination components to table down
    2. Create folder Topics/Edit with Body.razor that represents Topic Edit page
    3. Create PersonTopicSelectTable.razor component that is People Table with checkboxes. It used to assign people to specific topic

--- Pds/Pds.Api.Contracts/Topic/CheckBoxPersonDto.cs
+++ Pds/Pds.Api.Contracts/Topic/CheckBoxPersonDto.cs
@@ -1,11 +1,20 @@
-﻿using System;
-using Pds.Api.Contracts.Person;
+﻿using Pds.Api.Contracts.Person;
 
 namespace Pds.Api.Contracts.Topic
 {
-    public class CheckBoxPersonDto
+    public class CheckBoxPersonDto : PersonDto
     {
-        public PersonDto Person { get; set; }
+        public CheckBoxPersonDto(PersonDto personDto)
+        {
+            Id = personDto.Id;
+            FullName = personDto.FullName;
+            Location = personDto.Location;
+            Info = personDto.Info;
+            Rate = personDto.Rate;
+            Status = personDto.Status;
+            Resources = personDto.Resources;
+            Contents = personDto.Contents;
+        }
 
         public bool IsSelected { get; set; }
     }
