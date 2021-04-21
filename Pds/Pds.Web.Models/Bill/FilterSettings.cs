6bd9a46be6bc3c0304b0d71e911b52cb4c3fe9d0
itbeard <iamitbeard@gmail.com>
2021-04-21 15:01:29 +0300

    #121- filter for bills

--- Pds/Pds.Web.Models/Bill/FilterSettings.cs
+++ Pds/Pds.Web.Models/Bill/FilterSettings.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Pds.Web.Models.Bill
 {
@@ -9,5 +10,9 @@ namespace Pds.Web.Models.Bill
         public List<BillTypeFilterItem> BillTypeFilterItems { get; set; }
         public List<PaymentTypeFilterItem> PaymentTypeFilterItems { get; set; }
         public List<BrandFilterItem> BrandFilterItems { get; set; }
+
+        public DateTime? From { get; set; }
+
+        public DateTime? To { get; set; }
     }
 }
