02bf2809862891aeb214ebaaf9cb2333586d40c6
itbeard <iamitbeard@gmail.com>
2021-11-10 04:09:44 +0300

    Upgrade Pds.Web up to .NET 6

--- Pds/Pds.Web/Program.cs
+++ Pds/Pds.Web/Program.cs
@@ -1,34 +1,24 @@
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Pds.Web;
 using Pds.Web.Common;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
-namespace Pds.Web
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddTransient<IApiClient, ApiClient>();
+builder.Services.AddSingleton<PageHistoryState>();
+builder.Services.AddHeadElementHelper();
+builder.Services.AddCustomAutoMapper();
+
+builder.Services.AddOidcAuthentication(options =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+    builder.Configuration.Bind("Auth0", options.ProviderOptions);
+    options.ProviderOptions.ResponseType = "code";
+});
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddTransient<IApiClient, ApiClient>();
-            builder.Services.AddSingleton<PageHistoryState>();
-            builder.Services.AddHeadElementHelper();
-            builder.Services.AddCustomAutoMapper();
+await builder.Build().RunAsync();
 
-            builder.Services.AddOidcAuthentication(options =>
-            {
-                builder.Configuration.Bind("Auth0", options.ProviderOptions);
-                options.ProviderOptions.ResponseType = "code";
-            });
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
