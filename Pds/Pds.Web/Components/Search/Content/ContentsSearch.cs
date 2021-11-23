a851845b7d665ac4fabb2deb3ff4131c39133c20
itbeard <iamitbeard@gmail.com>
2021-11-23 14:57:45 +0300

    fix Cintents searching predicates and refactoring

--- Pds/Pds.Web/Components/Search/Content/ContentsSearch.cs
+++ Pds/Pds.Web/Components/Search/Content/ContentsSearch.cs
@@ -13,18 +13,23 @@ public class ContentsSearch
         var predicate = PredicateBuilder.False<GetContentsContentDto>();
 
         predicate = predicate.Or(c => c.Title.ToLower().Contains(searchLine));
-        predicate = predicate.Or(c => c.ClientName.ToLower().Contains(searchLine));
-        predicate = predicate.Or(c => 
-            c.EndDate != null &&
-            !string.IsNullOrWhiteSpace(c.EndDate.Value.ToString("dd.MM.yyyy")) && 
-            c.EndDate.Value.ToString("dd.MM.yyyy").ToLower().Contains(searchLine));
-        predicate = predicate.Or(c => 
-            !string.IsNullOrWhiteSpace(c.ReleaseDate.ToString("dd.MM.yyyy")) && 
-            c.ReleaseDate.ToString("dd.MM.yyyy").ToLower().Contains(searchLine));
+        predicate = predicate.Or(c => c.ClientName != null 
+                                      && c.ClientName.ToLower().Contains(searchLine));
+        predicate = predicate.Or(c => c.EndDate != null 
+                                      && !string.IsNullOrWhiteSpace(c.EndDate.Value.ToString("dd.MM.yyyy")) 
+                                      && c.EndDate.Value.ToString("dd.MM.yyyy").ToLower().Contains(searchLine));
+        predicate = predicate.Or(c => !string.IsNullOrWhiteSpace(c.ReleaseDate.ToString("dd.MM.yyyy")) 
+                                      && c.ReleaseDate.ToString("dd.MM.yyyy").ToLower().Contains(searchLine));
 
-        predicate = predicate.Or(r => r.Brand != null && r.Brand.Name.ToLower().Contains(searchLine));
-        predicate = predicate.Or(r => r.Person != null && r.Person.FirstName.ToLower().Contains(searchLine));
-        predicate = predicate.Or(r => r.Person != null && r.Person.LastName.ToLower().Contains(searchLine));
+        predicate = predicate.Or(r => r.Brand != null 
+                                      && r.Brand.Name != null
+                                      && r.Brand.Name.ToLower().Contains(searchLine));
+        predicate = predicate.Or(r => r.Person != null 
+                                      && r.Person.FirstName != null 
+                                      && r.Person.FirstName.ToLower().Contains(searchLine));
+        predicate = predicate.Or(r => r.Person != null 
+                                      && r.Person.LastName != null
+                                      && r.Person.LastName.ToLower().Contains(searchLine));
         predicate = predicate.Or(r => r.Person != null 
                                       && r.Person.ThirdName != null 
                                       && r.Person.ThirdName.ToLower().Contains(searchLine));
