046b3f494fe0f1e1fe24ad05e722977f675d7ca5
itbeard <iamitbeard@gmail.com>
2022-01-14 19:37:32 +0200

    #161 - add filter by ReleaseDate to Content page

--- Pds/Pds.Web.Models/Content/FilterSettings.cs
+++ Pds/Pds.Web.Models/Content/FilterSettings.cs
@@ -5,4 +5,8 @@ public class FilterSettings
     public List<SocialMediaFilterItem> SocialMediaFilterItems { get; set; }
     public List<ContentTypeFilterItem> ContentTypeFilterItems { get; set; }
     public List<BrandFilterItem> BrandFilterItems { get; set; }
+    
+    public DateTime? From { get; set; }
+
+    public DateTime? To { get; set; }
 }
