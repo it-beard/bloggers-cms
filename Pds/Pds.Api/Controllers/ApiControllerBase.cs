b644416db2db2dacdd6b63dedb8dfa123bb89526
itbeard <iamitbeard@gmail.com>
2022-07-19 02:30:33 +0200

    update errors messeging

--- Pds/Pds.Api/Controllers/ApiControllerBase.cs
+++ Pds/Pds.Api/Controllers/ApiControllerBase.cs
@@ -12,12 +12,16 @@ public abstract class ApiControllerBase : ControllerBase
         return exception switch
         {
             _ when exception is RepositoryException => 
-                StatusCode((int) HttpStatusCode.BadRequest, $"DB error: {exception.Message}"),
+                StatusCode((int) HttpStatusCode.BadRequest, $"DB error: {exception.Message}" + 
+                                                            Environment.NewLine + 
+                                                            $"{exception.InnerException.Message}"),
             _ when exception is IApiException apiException => 
                 StatusCode((int) HttpStatusCode.BadRequest, apiException.Errors),
 
             _ => 
-                StatusCode((int) HttpStatusCode.BadRequest, $"Smtg went wrong: {exception.Message}")
+                StatusCode((int) HttpStatusCode.BadRequest, $"Smtg went wrong: {exception.Message}" + 
+                                                            Environment.NewLine + 
+                                                            $"{exception.InnerException.Message}")
         };
     }
 }
