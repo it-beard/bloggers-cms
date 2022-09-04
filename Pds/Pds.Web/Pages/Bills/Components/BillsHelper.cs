79a6599348269057bf75437e3cc90d919da9bcf8
itbeard <iamitbeard@gmail.com>
2022-09-04 16:54:29 +0200

    #227 - add gray selection to Bills\Costs archived items

---
+++ Pds/Pds.Web/Pages/Bills/Components/BillsHelper.cs
@@ -0,0 +1,16 @@
+﻿using Pds.Core.Enums;
+
+namespace Pds.Web.Pages.Bills.Components;
+
+public static class BillsHelper
+{
+    public static string GetBgColorClass(BillStatus status)
+    {
+        return status switch
+        {
+            BillStatus.Active => "active",
+            BillStatus.Archived => "archived",
+            _ => string.Empty
+        };
+    }
+}
---
+++ Pds/Pds.Web/Pages/Costs/Components/CostsHelper.cs
@@ -0,0 +1,16 @@
+﻿using Pds.Core.Enums;
+
+namespace Pds.Web.Pages.Costs.Components;
+
+public static class CostsHelper
+{
+    public static string GetBgColorClass(CostStatus status)
+    {
+        return status switch
+        {
+            CostStatus.Active => "active",
+            CostStatus.Archived => "archived",
+            _ => string.Empty
+        };
+    }
+}
