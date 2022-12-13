1b639b0a748453026781979d365f1f533b4d2473
itbeard <iamitbeard@gmail.com>
2022-12-13 21:37:03 +0100

    update get persons list

--- Pds/Pds.Data/Repositories/PersonRepository.cs
+++ Pds/Pds.Data/Repositories/PersonRepository.cs
@@ -9,23 +9,24 @@ namespace Pds.Data.Repositories;
 public class PersonRepository : RepositoryBase<Person>, IPersonRepository
 {
     private readonly ApplicationDbContext context;
-        
+
     public PersonRepository(ApplicationDbContext context) : base(context)
     {
         this.context = context;
     }
-        
+
     public async Task<List<Person>> GetAllFullAsync()
     {
         return await context.Persons
             .Include(p=>p.Brands)
             .Include(p=>p.Resources)
             .Include(p=>p.Contents)
+            .Where(p=>p.Brands.Any(b=>!b.IsArchived))
             .OrderBy(p =>p.LastName)
             .ThenByDescending(p=>p.Rate)
             .ToListAsync();
     }
-        
+
     public async Task<Person> GetFullByIdAsync(Guid personId)
     {
         return await context.Persons
@@ -34,12 +35,12 @@ public class PersonRepository : RepositoryBase<Person>, IPersonRepository
             .Include(p=>p.Contents)
             .FirstOrDefaultAsync(c => c.Id == personId);
     }
-        
+
     public async Task<List<Person>> GetForListByBrandId(Guid brandId)
     {
         return await context.Persons
-            .Where(p => 
-                p.Brands.Select(b=>b.Id).Contains(brandId) && 
+            .Where(p =>
+                p.Brands.Select(b=>b.Id).Contains(brandId) &&
                 p.Status != PersonStatus.Archived &&
                 p.FirstName != PersonConstants.UnknownPersonMarker &&
                 p.LastName != PersonConstants.UnknownPersonMarker)
